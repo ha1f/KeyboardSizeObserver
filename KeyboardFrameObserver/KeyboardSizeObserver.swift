de407d2b25ea2aa8fd4bae9449b8c5c1e225143a
ha1f <ha1f@linecorp.com>
2019-02-08 18:42:42 +0900

    Implemented

---
+++ KeyboardFrameObserver/KeyboardSizeObserver.swift
@@ -0,0 +1,92 @@
+//
+//  KeyboardSizeObserver.swift
+//  KeyboardFrameObserver
+//
+//  Created by ha1f on 2019/02/08.
+//  Copyright © 2019年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+protocol KeyboardSizeObserverDelegate: AnyObject {
+    func keyboardSizeObserver(_ observer: KeyboardSizeObserver, sizeDidChangeTo size: CGSize)
+}
+
+extension KeyboardSizeObserverDelegate {
+    func keyboardSizeObserver(_ observer: KeyboardSizeObserver, sizeDidChangeTo size: CGSize) {
+        // implementation is optional
+    }
+}
+
+/// This class have to be instantiated before keyboard shown to track keyboard correctly.
+/// userInfo returns frame, but doc recommends us not to use origin, and just use size.
+/// Ref: https://developer.apple.com/library/archive/documentation/StringsTextFonts/Conceptual/TextAndWebiPhoneOS/KeyboardManagement/KeyboardManagement.html
+final class KeyboardSizeObserver: NSObject {
+    /// Note that we have to set this property using _dispatchQueue
+    public private(set) var keyboardSize: CGSize = KeyboardSizeObserver._defaultSize {
+        didSet {
+            delegate?.keyboardSizeObserver(self, sizeDidChangeTo: keyboardSize)
+        }
+    }
+    
+    public weak var delegate: KeyboardSizeObserverDelegate? {
+        didSet {
+            _dispatchQueue.async { [self, delegate, keyboardSize] in
+                // notify initial value
+                delegate?.keyboardSizeObserver(self, sizeDidChangeTo: keyboardSize)
+            }
+        }
+    }
+    
+    /// For thread safety, we have to use at least serial queue
+    private let _dispatchQueue: DispatchQueue = DispatchQueue.main
+    
+    private var _keyboardFrameEndKey: String {
+        #if swift(>=4.2)
+        return UIResponder.keyboardFrameEndUserInfoKey
+        #else
+        return UIKeyboardFrameEndUserInfoKey
+        #endif
+    }
+    
+    private static let _defaultSize = CGSize(
+        width: UIScreen.main.bounds.width,
+        height: 0
+    )
+    
+    override init() {
+        super.init()
+        
+        // Though we have more notifications, since [RxKeyboard](https://github.com/RxSwiftCommunity/RxKeyboard/blob/master/Sources/RxKeyboard/RxKeyboard.swift) uses
+        // only this two, I guess this is enough
+        #if swift(>=4.2)
+        let keyboardWillChangeFrame = UIResponder.keyboardWillChangeFrameNotification
+        let keyboardWillHide = UIResponder.keyboardWillHideNotification
+        #else
+        let keyboardWillChangeFrame = NSNotification.Name.UIKeyboardWillChangeFrame
+        let keyboardWillHide = NSNotification.Name.UIKeyboardWillHide
+        #endif
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceivedKeyboardWillHideNotification(_:)), name: keyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceivedKeyboardWillChangeFrameNotification(_:)), name: keyboardWillChangeFrame, object: nil)
+    }
+    
+    private func _handleNotification(_ notification: Notification) {
+        let newSize = (notification.userInfo?[_keyboardFrameEndKey] as? NSValue)?.cgRectValue.size
+            ?? KeyboardSizeObserver._defaultSize
+        
+        _dispatchQueue.async { [weak self] in
+            self?.keyboardSize = newSize
+        }
+    }
+    
+    @objc
+    private func didReceivedKeyboardWillChangeFrameNotification(_ notification: Notification) {
+        _handleNotification(notification)
+    }
+    
+    @objc
+    private func didReceivedKeyboardWillHideNotification(_ notification: Notification) {
+        _handleNotification(notification)
+    }
+}
--- KeyboardFrameObserver/ViewController.swift
+++ KeyboardFrameObserver/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
+/// 大体の画面に対しておすすめの構造。
+/// ScrollView > UIView > UIStackView > その他のViewたち
+/// UIViewはScrollViewに対してLeading, Trailing, Top, Bottom EdgesをAlignして、
+/// Equal Widthもつける。
+/// UIStackViewはUIViewに対してLeading, Trailing, Top, Bottom EdgesをAlign
+/// 要素はUIStackViewの小要素として配置。
+///
+/// UIStackViewはAlignmentをcenterにして、
+/// 子要素は幅と高さだけconstraintすればOK。
+/// SafeAreaからはみ出したくない場合は、SafeAreaのLeading, Trailing Edgesと高さでもよい。
+/// 固有の背景をつけたい場合は一旦UIViewをおいて、それをUIStackViewとEqual Widthと高さをセットする。
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scrollViewContentView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    
+    let keyboardSizeObserver = KeyboardSizeObserver()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.contentInsetAdjustmentBehavior = .never
+        keyboardSizeObserver.delegate = self
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // dismissとかで閉じるときにこれがないとキーボードが残ってちょっとダサい
+        textField.resignFirstResponder()
+    }
+    
+    override func viewSafeAreaInsetsDidChange() {
+        super.viewSafeAreaInsetsDidChange()
+        _updateScrollViewContentInsets(keyboardSize: keyboardSizeObserver.keyboardSize)
+    }
+    
+    private func _updateScrollViewContentInsets(keyboardSize: CGSize) {
+        let newInsets = UIEdgeInsets(
+            top: view.safeAreaInsets.top,
+            left: 0,
+            bottom: max(keyboardSize.height, view.safeAreaInsets.bottom),
+            right: 0
+        )
+        scrollView.contentInset = newInsets
+        // こっちも更新しないとindicatorが変になる
+        scrollView.scrollIndicatorInsets = newInsets
+    }
+}
 
-
+extension ViewController: KeyboardSizeObserverDelegate {
+    func keyboardSizeObserver(_ observer: KeyboardSizeObserver, sizeDidChangeTo size: CGSize) {
+        _updateScrollViewContentInsets(keyboardSize: size)
+    }
 }
 
